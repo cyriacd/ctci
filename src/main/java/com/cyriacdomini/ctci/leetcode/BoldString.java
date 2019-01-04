2501626b7e2fc8c22503ec15465101b3ac856613
Isaac Domini <me@isaacdomini.com>
2019-01-04 12:59:32 -0500

    Update BoldString.java

--- src/main/java/com/cyriacdomini/ctci/leetcode/BoldString.java
+++ src/main/java/com/cyriacdomini/ctci/leetcode/BoldString.java
@@ -15,24 +15,21 @@ public class BoldString{
             }
     
         }
-        int count = 1, previ = -1;
-        while(count > 0){
-            count = 0;
-            for(int i = 0; i < matchIndex.size(); i++){
-                List<Integer> curr = matchIndex.get(i);
-                if(curr.get(0)<=previ && i>0){
-                    List<Integer> prev = matchIndex.get(i-1);
-                    prev.set(1, curr.get(1));
-                    matchIndex.remove(i);
-                    count++;
-                    i--;                        
-                }
-                previ = curr.get(1);
+        int previ = -1;
+        for(int i = 0; i < matchIndex.size(); i++){
+            List<Integer> curr = matchIndex.get(i);
+            if(curr.get(0)<=previ && i>0){
+                List<Integer> prev = matchIndex.get(i-1);
+                prev.set(1, curr.get(1));
+                matchIndex.remove(i);
+                i--;                        
             }
+            previ = curr.get(1);
         }
         for(int i = matchIndex.size()-1;i>=0; i--){
             s = s.substring(0,matchIndex.get(i).get(1)) + "</b>" + s.substring(matchIndex.get(i).get(1),s.length()); 
             s = s.substring(0,matchIndex.get(i).get(0)) + "<b>" + s.substring(matchIndex.get(i).get(0),s.length()); 
+//             s = s.substring(0,
         }
         return s;
     }
